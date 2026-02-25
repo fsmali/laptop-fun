b8ca0643eb025174e9c6d2dab94835c1ad103ec6
Ali Dede <alidede@Alis-MacBook-Pro.local>
2026-02-25 22:44:14 +0000

    debug

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -63,7 +63,7 @@ export default function Experience() {
         <Text
           font="./bangers-v20-latin-regular.woff"
           fontSize={isMobile ? 0.3 : 0.6}
-          position={isMobile ? [0, 0.6, 0.75] : [0, 1.35, 0.75]}
+          position={isMobile ? [0, 1, 0.75] : [0, 1.35, 0.75]}
           // rotation-y={-1.25}
           maxWidth={2}
         >
