326f72f6a16198c98a6c7e20de062219c80c7004
Ali Dede <alidede@Alis-MacBook-Pro.local>
2026-02-26 12:46:05 +0000

    solved deploy issue1

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import './style.css';
 import ReactDOM from 'react-dom/client';
 
-import App from './app.js';
+import App from './App.jsx';
 
 const root = ReactDOM.createRoot(document.querySelector('#root'));
 
